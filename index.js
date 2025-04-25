f44d5c919d7b26fb4b14dcdf2b94cc214ec4451d
nurainande <nurainande@gmail.com>
2025-04-25 17:51:53 +0100

    correct

--- index.js
+++ index.js
@@ -14,28 +14,29 @@ dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
-const __dirname = path.resolve();
+// const __dirname = path.resolve();
 
 app.use(cors({ origin: `${process.env.CLIENT_URL}`, credentials: true }));
 
 app.use(express.json()); // allows us to parse incoming requests:req.body
 app.use(cookieParser()); // allows us to parse incoming cookies
 
+app.get('/musa', (req, res) => {
+    res.send('Hello, World MusaSSSSASSS and the developers !');
+});
+
 app.use("/api/auth", authRoutes);
 app.use("/api/investments", investmentsRoutes);
 
 
-if (process.env.NODE_ENV === "production") {
-    app.use(express.static(path.join(__dirname, "/frontend/dist")));
+// if (process.env.NODE_ENV === "production") {
+//     app.use(express.static(path.join(__dirname, "/frontend/dist")));
 
-    app.get("*", (req, res) => {
-        res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
-    });
-}
+//     app.get("*", (req, res) => {
+//         res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+//     });
+// }
 // Basic route
-app.get('/', (req, res) => {
-    res.send('Hello, World MusaSSSSASSS and the developers !');
-});
 
 app.listen(PORT, () => {
     connectDB();
